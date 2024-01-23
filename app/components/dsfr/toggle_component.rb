80a5d94344834e35a14d642fb71d319b9eff9ba8
Eric Leroy-Terquem <eric.leroyterquem@beta.gouv.fr>
2024-01-23 11:16:51 +0100

    style : update toggle component

--- app/components/dsfr/toggle_component.rb
+++ app/components/dsfr/toggle_component.rb
@@ -1,9 +1,12 @@
 class Dsfr::ToggleComponent < ApplicationComponent
-  def initialize(form:, target:, title:, hint:, disabled:)
+  def initialize(form:, target:, title:, disabled: nil, hint: nil, toggle_labels: { checked: 'Activé', unchecked: 'Désactivé' })
     @form = form
     @target = target
     @title = title
     @hint = hint
     @disabled = disabled
+    @toggle_labels = toggle_labels
   end
+
+  attr_reader :toggle_labels
 end
